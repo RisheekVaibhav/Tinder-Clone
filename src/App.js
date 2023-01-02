0ae5664db217d9aff406af8009a7342583b8f192
Risheek <risheekjk@gmail.com>
2023-01-03 00:19:39 +0530

    Nice

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './App.css';
+import Header from "./Header";
+import TinderCards from "./TinderCards";
+
+
+function App() {
+  return (
+    //BEM class naming convention
+    <div className="app">
+      <Header />
+      <TinderCards />            
+      {/*SwipeButoons*/}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TinderCards.js
@@ -0,0 +1,38 @@
+
+import react, { useState } from 'react'
+import "./TinderCards.css";
+
+function TinderCards() {
+    const [people, setPeople] = useState([
+        {
+            name : 'Elon Musk',
+            url: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg/1200px-Elon_Musk_Royal_Society_%28crop2%29.jpg"
+        },
+        {
+            name : 'Lavya Bhasin',
+            url: "https://media.licdn.com/dms/image/D4D03AQH90UW9XnthpA/profile-displayphoto-shrink_200_200/0/1668238629520?e=1677715200&v=beta&t=Hhjx7FOzBV2BuRi4msXNQb3jpQzm2A0XF6mEIxRchX8"
+        },
+    ]);
+    const swiped = (direction,nameToDelete) => {
+        console.log("removing: "+ nameToDelete);
+    };
+    const outOfFrame = (name) => {
+        console.log(name+ "left the screen!")
+    };
+    return <div className='tinderCards'>  
+        <div className='div.tinderCards__cardContainers'>
+        {people.map((person)=>(
+            <TinderCard
+                className="swipe"
+                key={person.name}
+                preventSwipe={["up","down"]}
+                onSwipe={(dir) => swiped(dir,person.name)}
+                onCardLeftScreen={() => outOfFrame(person.name)}
+            ></TinderCard>
+        ))}
+        </div>       
+
+    </div>
+}
+
+export default TinderCards
