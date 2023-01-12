19371b66696561d231d0e1ba848a603e772a4aba
Risheek <risheekjk@gmail.com>
2023-01-12 20:13:22 +0530

    ?

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'https://splendid-gumption-2d1133.netlify.app'
+    baseURL: 'http://Amber Heard:8001/'
 })
 
 export default instance;
